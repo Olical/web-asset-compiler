f643953aa53f6b94a5e78202b1a91552af18ea8e
Oliver Caldwell <oliver@flowdev.co.uk>
2011-12-31 21:10:37 +0000

    Added the loading and parsing of the fileList

--- wac-binary.js
+++ wac-binary.js
@@ -12,7 +12,22 @@ program
 	.option('-l, --fileList [path]', 'Specify a diffent file list to the default assets.json')
 	.parse(process.argv);
 
+// Initialise the required global variables
+var fileList = null,
+	fileListPath = program.fileList || './assets.json';
+
 // Load the file list
-fs.readFile(program.fileList || './assets.json', 'utf8', function(err, data) {
-	
+fs.readFile(fileListPath, 'utf8', function(err, data) {
+	if(err) {
+		logme.error('The file list (' + fileListPath + ') could not be read.');
+	}
+	else {
+		// Decode the data
+		try {
+			fileList = JSON.parse(data);
+		}
+		catch(e) {
+			logme.error('The file list (' + fileListPath + ') is not valid JSON.');
+		}
+	}
 });
