85cc8c95c7dd1c7e2d55ff4ee0c3d39b28d3bebb
Oliver Caldwell <oliver@flowdev.co.uk>
2011-12-31 21:02:53 +0000

    Added the commander setup and reading of the config

--- wac-binary.js
+++ wac-binary.js
@@ -3,6 +3,15 @@
 // Load the required modules
 var fs = require('fs');
 var path = require('path');
-var commander = require('commander');
+var program = require('commander');
 
-// Check for the config
+// Set up commander
+program
+	.version('0.0.0')
+	.option('-l, --fileList [path]', 'Specify a diffent file list to the default assets.json')
+	.parse(process.argv);
+
+// Load the file list
+fs.readFile(program.fileList || './assets.json', 'utf8', function(err, data) {
+	
+});
