a910caed6843897de83f7adb04acc070a3ce29f5
Oliver Caldwell <oliver@flowdev.co.uk>
2011-12-31 22:04:01 +0000

    Added the asset object building

--- wac-binary.js
+++ wac-binary.js
@@ -13,8 +13,31 @@ program
 	.parse(process.argv);
 
 // Initialise the required global variables
-var fileList = null,
-	fileListPath = program.fileList || './assets.json';
+var fileListPath = program.fileList || './assets.json';
+
+/**
+ * Parses a files name and contents and drops the results into the specified object
+ * 
+ * @param {String} path The target files path
+ * @param {Object} target The object to store the parsed data in
+ * @param {Function} callback Called when done. Will pass the file and error object
+ */
+function parseFile(path, target, callback) {
+	// Load the files contents
+	fs.readFile(path, 'utf8', function(err, data) {
+		// Make sure there were no errors
+		if(!err) {
+			// Store the contents
+			target.content = data;
+			
+			// Calculate the type
+			target.type = /(?:\.([^.]+))?$/.exec(path)[1];
+		}
+		
+		// Run the callback
+		callback.call(null, path, err);
+	});
+}
 
 // Load the file list
 fs.readFile(fileListPath, 'utf8', function(err, data) {
@@ -24,7 +47,27 @@ fs.readFile(fileListPath, 'utf8', function(err, data) {
 	else {
 		// Decode the data
 		try {
-			fileList = JSON.parse(data);
+			var fileList = JSON.parse(data);
+			var fileObjects = [];
+			var doneCount = 0;
+			
+			// Convert the paths to objects
+			for(var i = 0; i < fileList.length; i += 1) {
+				fileObjects.push({});
+				parseFile(fileList[i], fileObjects[i], function(path, err) {
+					if(err) {
+						logme.error('The asset (' + path + ') could not be read.');
+					}
+					
+					// Increment the done count
+					doneCount += 1;
+					
+					// If it is one above the length of files then all are done
+					if(doneCount >= fileList.length) {
+						
+					}
+				});
+			}
 		}
 		catch(e) {
 			logme.error('The file list (' + fileListPath + ') is not valid JSON.');
